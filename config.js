1594ca5536b6be323f039a651d11843be77ec876
Awais Qadir <128619585+anhackers786@users.noreply.github.com>
2024-11-15 13:30:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "0flCAD7T#w5P88eVVRpcXvT08OHoFM_dXEYQBvM1mAFKEck7KBr0",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
